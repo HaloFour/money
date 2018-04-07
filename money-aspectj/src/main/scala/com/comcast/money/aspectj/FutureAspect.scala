e5f5c48da2ad92aec8fa91883a65b7eee9fcbd83
Spindler, Justin <Justin_Spindler@comcast.com>
2018-04-07 13:46:19 -0400

    Simplify FutureAspect

--- money-aspectj/src/main/scala/com/comcast/money/aspectj/FutureAspect.scala
+++ money-aspectj/src/main/scala/com/comcast/money/aspectj/FutureAspect.scala
@@ -16,142 +16,31 @@
 
 package com.comcast.money.aspectj
 
-import com.comcast.money.annotations.{ Timed, Traced }
+import java.util
+
+import com.comcast.money.api.Span
 import com.comcast.money.core.internal.{ MDCSupport, SpanLocal }
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.{ Around, Aspect, Pointcut }
 import org.slf4j.MDC
 
-import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.Try
+import scala.concurrent.ExecutionContext
 
 @Aspect
 class FutureAspect {
-  @Pointcut("execution(@com.comcast.money.annotations.Traced * *(..)) && @annotation(traceAnnotation)")
-  def traced(traceAnnotation: Traced): Unit = {}
+  @Pointcut("call(* scala.concurrent.Future$.apply(.., scala.concurrent.ExecutionContext)) && args(.., executionContext)")
+  def futureApplyExecutionContext(executionContext: ExecutionContext): Unit = {}
 
-  @Pointcut("execution(@com.comcast.money.annotations.Timed * *(..)) && @annotation(timedAnnotation)")
-  def timed(timedAnnotation: Timed): Unit = {}
+  @Pointcut("call(* scala.concurrent.Future+.*(.., scala.concurrent.ExecutionContext)) && args(.., executionContext)")
+  def futureMethodExecutionContext(executionContext: ExecutionContext): Unit = {}
 
-  /*
-  @Around("call(scala.concurrent.Future *(..)) && cflow(traced(traceAnnotation))")
-  def instrumentFuture(joinPoint: ProceedingJoinPoint, traceAnnotation: Traced): AnyRef = {
-    if (traceAnnotation.async()) {
-      joinPoint.proceed() match {
-        case future: Future[_] =>
-          future
-        case other => other
-      }
-    } else {
-      joinPoint.proceed()
-    }
-  }
-  */
+  @Around("futureApplyExecutionContext(executionContext) || futureMethodExecutionContext(executionContext)")
+  def instrumentFutureExecutionContext(joinpoint: ProceedingJoinPoint, executionContext: ExecutionContext): AnyRef =
+    joinpoint.proceed(replaceExecutionContext(executionContext, joinpoint.getArgs))
 
-  @Pointcut("call(* scala.concurrent.Future$.apply(..)) && args(body, executor)")
-  def futureApply[T](body: () => T, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.andThen(..)) && args(pf, executor)")
-  def futureAndThen[T, U](pf: PartialFunction[Try[T], U], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.collect(..)) && args(pf, executor)")
-  def futureCollect[T, S](pf: PartialFunction[T, S], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.filter(..)) && args(pred, executor)")
-  def futureFilter[T](pred: T => Boolean, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.flatMap(..)) && args(f, executor)")
-  def futureFlatMap[T, U](f: T => Future[U], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.foreach(..)) && args(f, executor)")
-  def futureForEach[T, U](f: T => U, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.map(..)) && args(f, executor)")
-  def futureMap[T, U](f: T => U, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.onComplete(..)) && args(f, executor)")
-  def futureOnComplete[T, U](f: Try[T] => U, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.onFailure(..)) && args(pf, executor)")
-  def futureOnFailure[T, U](pf: PartialFunction[Throwable, U], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.onSuccess(..)) && args(pf, executor)")
-  def futureOnSuccess[T, U](pf: PartialFunction[T, U], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.recover(..)) && args(pf, executor)")
-  def futureRecover[T, U](pf: PartialFunction[Throwable, U], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.recoverWith(..)) && args(pf, executor)")
-  def futureRecoverWith[T, U](pf: PartialFunction[Throwable, Future[U]], executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.transform(..)) && args(s, f, executor)")
-  def futureTransform[T, S](s: T => S, f: Throwable => Throwable, executor: ExecutionContext): Unit = {}
-
-  @Pointcut("call(* scala.concurrent.Future.withFilter(..)) && args(p, executor)")
-  def futureWithFilter[T](p: T => Boolean, executor: ExecutionContext): Unit = {}
-
-  @Around("futureApply(body, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureApply[T](joinpoint: ProceedingJoinPoint, body: () => T, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(body, wrapExecutionContext(executor)) }
-
-  @Around("futureAndThen(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureAndThen[T, U](joinpoint: ProceedingJoinPoint, pf: PartialFunction[Try[T], U], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureCollect(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureCollect[T, S](joinpoint: ProceedingJoinPoint, pf: PartialFunction[T, S], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureFilter(pred, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureFilter[T](joinpoint: ProceedingJoinPoint, pred: T => Boolean, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pred, wrapExecutionContext(executor)) }
-
-  @Around("futureFlatMap(f, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureFlatMap[T, U](joinpoint: ProceedingJoinPoint, f: T => Future[U], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(f, wrapExecutionContext(executor)) }
-
-  @Around("futureForEach(f, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureForEach[T, U](joinpoint: ProceedingJoinPoint, f: T => U, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(f, wrapExecutionContext(executor)) }
-
-  @Around("futureMap(f, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureMap[T, U](joinpoint: ProceedingJoinPoint, f: T => U, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(f, wrapExecutionContext(executor)) }
-
-  @Around("futureOnComplete(f, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureOnComplete[T, U](joinpoint: ProceedingJoinPoint, f: Try[T] => U, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(f, wrapExecutionContext(executor)) }
-
-  @Around("futureOnFailure(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureOnFailure[T, U](joinpoint: ProceedingJoinPoint, pf: PartialFunction[Throwable, U], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureOnSuccess(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureOnSuccess[T, U](joinpoint: ProceedingJoinPoint, pf: PartialFunction[T, U], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureRecover(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureRecover[T, U](joinpoint: ProceedingJoinPoint, pf: PartialFunction[Throwable, U], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureRecoverWith(pf, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureRecoverWith[T, U](joinpoint: ProceedingJoinPoint, pf: PartialFunction[Throwable, Future[U]], executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(pf, wrapExecutionContext(executor)) }
-
-  @Around("futureTransform(s, f, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureTransform[T, S](joinpoint: ProceedingJoinPoint, s: T => S, f: Throwable => Throwable, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(s, f, wrapExecutionContext(executor)) }
-
-  @Around("futureWithFilter(p, executor) && cflow(traced(traceAnnotation))")
-  def instrumentFutureWithFilter[T](joinpoint: ProceedingJoinPoint, p: T => Boolean, executor: ExecutionContext, traceAnnotation: Traced): AnyRef =
-    instrumentAsync(joinpoint, traceAnnotation) { Array(p, wrapExecutionContext(executor)) }
-
-  private def instrumentAsync(joinpoint: ProceedingJoinPoint, traceAnnotation: Traced)(f: => Array[AnyRef]) = {
-    if (traceAnnotation.async()) {
-      joinpoint.proceed(f)
-    } else {
-      joinpoint.proceed()
-    }
+  def replaceExecutionContext(executionContext: ExecutionContext, args: Array[AnyRef]): Array[AnyRef] = {
+    args.update(args.length - 1, wrapExecutionContext(executionContext))
+    args
   }
 
   private lazy val mdcSupport = new MDCSupport()
@@ -159,21 +48,30 @@ class FutureAspect {
   private def wrapExecutionContext(executionContext: ExecutionContext): ExecutionContext =
     SpanLocal.current.map {
       span =>
-        val mdc = Option(MDC.getCopyOfContextMap)
-        new ExecutionContext {
-          override def execute(runnable: Runnable): Unit = {
-            val previous = Option(MDC.getCopyOfContextMap)
-            try {
-              SpanLocal.push(span)
-              mdcSupport.propogateMDC(mdc)
-              runnable.run()
-            } finally {
-              SpanLocal.pop()
-              mdcSupport.propogateMDC(previous)
-            }
-          }
-
-          override def reportFailure(cause: Throwable): Unit = executionContext.reportFailure(cause)
-        }
+        new SpanRestoringExecutionContext(executionContext, span, Option(MDC.getCopyOfContextMap))
     }.getOrElse(executionContext)
+
+  private class SpanRestoringExecutionContext(wrappee: ExecutionContext, span: Span, mdc: Option[util.Map[_, _]]) extends ExecutionContext {
+    override def execute(runnable: Runnable): Unit =
+      wrappee.execute(new Runnable {
+        override def run(): Unit =
+          withTracingContext { runnable.run() }
+      })
+
+    override def reportFailure(cause: Throwable): Unit =
+      withTracingContext { wrappee.reportFailure(cause) }
+
+    private def withTracingContext[T](f: => T): T = {
+      val previousMdc = Option(MDC.getCopyOfContextMap)
+
+      SpanLocal.push(span)
+      mdcSupport.propogateMDC(mdc)
+      try {
+        f
+      } finally {
+        SpanLocal.pop()
+        mdcSupport.propogateMDC(previousMdc)
+      }
+    }
+  }
 }
